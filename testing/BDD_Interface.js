1f0b26d9acc84c5a9cdbb3226a73124c8e41fee8
Spencer White <swhit210@ford.com>
2020-12-18 13:16:00 -0500

    BDD mocha interface

---
+++ testing/BDD_Interface.js
@@ -0,0 +1,24 @@
+describe('Test Suite', function() {
+  before(function() {
+    // ... Single Execution
+  });
+  
+  beforeEach(function() {
+    // ... Multiple Execution
+  });
+  
+  after(function() {
+    // ... Single Execution
+  });
+  
+  afterEach(function() {
+    // ... Multiple Execution
+  });
+
+  describe('Group()', function() {
+    context('Description', function() {
+      it('Test', function() {
+      });
+    });
+  });
+});
