a9cc9920c1c7766bd31cec75a36e36b576586be9
Spencer White <swhit210@ford.com>
2020-12-18 13:55:33 -0500

    added example joi validations

---
+++ npm/joi-validations.js
@@ -0,0 +1,20 @@
+const Joi = require("@hapi/joi");
+
+const objectOne = Joi.object().required();
+const objectTwo = Joi.object();
+const objectThree = Joi.object();
+
+const objectSchema = Joi.object().keys({
+    firstName: Joi.string().min(392).max(392).required(),
+    age: Joi.number().required(),
+  });
+
+const arraySchema = Joi.object().keys({
+  credentials: Joi.array().items(objectOne, objectTwo, objectThree),
+});
+
+const uuidSchema = Joi.string()
+  .guid({
+    version: ["uuidv4", "uuidv5"],
+  })
+  .required();
