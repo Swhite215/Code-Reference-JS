35d510fa17ce3ed0aed3a7f08ec2dc8829389dda
Spencer White <swhit210@ford.com>
2020-12-18 13:01:41 -0500

    example class service

---
+++ node/servers/services/classService.js
@@ -0,0 +1,16 @@
+//A service should operate indepedently in order to be modular in its application
+class SomethingService {
+    constructor() {}
+
+    getSomething() {}
+
+    getSomethings() {}
+
+    updateSomething() {}
+
+    createSomething() {}
+
+    deleteSomething() {}
+}
+
+module.exports = SomethingService;
