87674d2379798776ed7833d60d9609be1b4b373a
Spencer White <swhit210@ford.com>
2020-12-28 14:09:31 -0500

    notes on classes

---
+++ typescript/classes.ts
@@ -0,0 +1,132 @@
+// Class
+class Greeter {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeterV2 = new Greeter("world");
+
+// Inheritance
+class Animal {
+    #name: string; // Private Field
+    constructor(theName: string) {
+        this.#name = theName;
+    }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.#name} moved ${distanceInMeters}m.`);
+}
+}
+
+class Snake extends Animal {
+    constructor(name: string) {
+        super(name); // Must call super
+    }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
+    move(distanceInMeters = 45) {
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
+
+// Public (Anywhere), Private (Within Class), and Protected (Within Derived Classes) Modifiers
+// Read Only
+// Parameter Properties - constructor(readonly name: type) {}
+// Accessors
+
+const fullNameMaxLength = 10;
+
+class Employee {
+  private _fullName: string = "";
+
+  get fullName(): string {
+    return this._fullName;
+  }
+
+  set fullName(newName: string) {
+    if (newName && newName.length > fullNameMaxLength) {
+      throw new Error("fullName has a max length of " + fullNameMaxLength);
+    }
+
+    this._fullName = newName;
+  }
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+
+if (employee.fullName) {
+  console.log(employee.fullName);
+}
+
+// Static Properties
+class Grid {
+    static origin = { x: 0, y: 0 }; // Declare static
+
+    calculateDistanceFromOrigin(point: { x: number; y: number }) {
+        let xDist = point.x - Grid.origin.x; // Access static
+        let yDist = point.y - Grid.origin.y;
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+
+    constructor(public scale: number) {}
+}
+  
+let grid1 = new Grid(1.0); // 1x scale
+let grid2 = new Grid(5.0); // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+
+// Abstract Classes
+abstract class Department {
+
+    constructor(public name: string) {}
+
+    printName(): void {
+        console.log("Department name: " + this.name);
+    }
+
+    abstract printMeeting(): void; // must be implemented in derived classes
+}
+
+class AccountingDepartment extends Department {
+    constructor() {
+        super("Accounting and Auditing"); // constructors in derived classes must call super()
+    }
+
+    printMeeting(): void {
+        console.log("The Accounting Department meets each Monday at 10am.");
+    }
+
+    generateReports(): void {
+        console.log("Generating accounting reports...");
+    }
+}
+  
+let department: Department; // ok to create a reference to an abstract type
+department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
+department.printName();
+department.printMeeting();
+// department.generateReports(); // Why can't I do this?
